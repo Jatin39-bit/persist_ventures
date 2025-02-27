f2ec5834b6db7c214602eb2e9e7002978cb2a622
Jatin39-bit <techrevain@gmail.com>
2025-02-27 18:51:20 +0530

    Update middleware.js

--- src/middleware.js
+++ src/middleware.js
@@ -1,3 +1,5 @@
+export const runtime = "nodejs";
+
 import { NextResponse } from "next/server";
 import { cookies } from "next/headers";
 import { jwtVerify } from "jose";
@@ -43,3 +45,5 @@ export const config = {
         '/upload',
     ]
 }
+
+
